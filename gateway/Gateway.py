6becb5e6afd3b0bea901a5cddf2c7edb25c2e8aa
Lukas Baier <lukasbaier@outlook.de>
2024-02-04 16:42:44 +0100

    did not set state to 2

--- gateway/Gateway.py
+++ gateway/Gateway.py
@@ -144,8 +144,6 @@ class DiscordGateway:
             print(f"{datetime.now()} State failure, cannot grab heartbeat, tearing down", flush=True)
             return False
 
-        self.__state = 2
-
         self.__heartbeat_interval = ctx["heartbeat_interval"] // 1000
         if not self.__secsleep(int(random() * self.__heartbeat_interval)):
             return False
@@ -156,6 +154,8 @@ class DiscordGateway:
                 "op": 1,
                 "d": self.__s
             })
+
+        self.__state = 2
 
         return True
 
