f8c91260ddd1692c29848812b9bec0d2228c795f
lukas <lukasbaier@outlook.de>
2024-02-19 19:57:38 +0100

    fixed the uptime message

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def uptime(ctx):
     conv = datetime(1970, 1, 1, 0, 0) + delta
 
     bot.send_dm(ctx["author"]["id"], f"Online since {conv.month - 1} months, {conv.day - 1} days, "
-                                     f"{conv.hour} hours, {conv.minute} minutes and {conv.second} seconds")
+                                     f"{conv.hour - 1} hours, {conv.minute} minutes and {conv.second} seconds")
 
 
 bot.run_forever()
