f2e8677ad6ab8029bdb41e0e2d629e216f6c1092
Lukas Baier <lukasbaier@outlook.de>
2024-02-19 11:27:05 +0100

    updated the uptime method

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from json import loads
 from api.Bot import Bot, messages, reactions
 
+from datetime import datetime
+
 config = loads(open("config.json", "r").read())
 bot = Bot()
 
@@ -9,7 +11,11 @@ bot.set_token(config["token"])
 
 @messages.command('!')
 def uptime(ctx):
-    bot.send_dm(ctx["author"]["id"], f"Online since {bot.uptime}")
+    delta = datetime.now() - bot.uptime
+    conv = datetime(1970, 1, 1) + delta
+
+    bot.send_dm(ctx["author"]["id"], f"Online since {conv.month - 1} months, {conv.day - 1} days, {conv.hour} hours,"
+                                     f" {conv.minute} minutes and {conv.second} seconds")
 
 
 bot.run_forever()
